d540d6254ee6e34603b2f33362bd2023a72e2c0b
Vadim Voitenko <mirvoy8031@gmail.com>
2023-09-15 21:51:23 +0300

    Add Tags Bar

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -19,7 +19,11 @@ export class HomeComponent implements OnInit {
         this.foods = this.foodService.getAllFoodsBySearchTerm(
           params.searchTerm
         );
-      else this.foods = foodService.getAll();
+      else if (params.tag) {
+        this.foods = this.foodService.getAllFoodsByTag(params.tag);
+      } else {
+        this.foods = foodService.getAll();
+      }
     });
   }
 
