ce2349b93354c32922464a121aa99c75de1fa865
Vadim Voitenko <mirvoy8031@gmail.com>
2023-09-15 22:06:34 +0300

    Add Cart Page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { RatingModule } from 'ng-starrating';
-import { SearchComponent } from './partials/search/search.component';
-import { TagsComponent } from './partials/tags/tags.component';
+import { SearchComponent } from './components/partials/search/search.component';
+import { TagsComponent } from './components/partials/tags/tags.component';
 import { FoodPageComponent } from './components/pages/food-page/food-page.component';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { TitleComponent } from './components/partials/title/title.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,8 @@ import { FoodPageComponent } from './components/pages/food-page/food-page.compon
     SearchComponent,
     TagsComponent,
     FoodPageComponent,
+    CartPageComponent,
+    TitleComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, RatingModule],
   providers: [],
