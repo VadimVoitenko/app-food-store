ee48622152d6d08b8b755530e4cd169aee322a9c
Vadim Voitenko <mirvoy8031@gmail.com>
2023-09-16 23:27:42 +0300

    Add Login Page

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import { sample_foods, sample_tags } from './data';
+import { sample_foods, sample_tags, sample_users } from './data';
+import jsonwebtoken from 'jsonwebtoken';
 
 const app = express();
+app.use(express.json());
 
 app.use(
   cors({
@@ -38,6 +40,34 @@ app.get('/api/foods/:foodId', (request, response) => {
   const food = sample_foods.find((food) => food.id == foodId);
   response.send(food);
 });
+
+app.post('/api/users/login', (request, response) => {
+  const { email, password } = request.body;
+  const user = sample_users.find(
+    (user) => user.email === email && user.password === password
+  );
+  if (user) {
+    response.send(generateTokenResponse(user));
+  } else {
+    response.status(400).send('User name or password is not valid!');
+  }
+});
+
+const generateTokenResponse = (user: any) => {
+  const token = jsonwebtoken.sign(
+    {
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    'SomeRandomText',
+    {
+      expiresIn: '30d',
+    }
+  );
+
+  user.token = token;
+  return user;
+};
 
 const port = 5000;
 app.listen(port, () => {
